d52bd22c0e5db1dbfc65426c981d027984b3da67
bellesouza <127145848+bellesouza@users.noreply.github.com>
2024-04-26 23:07:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 25;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'gato bobo', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 25');
 }
 
 exibirMensagemInicial();
